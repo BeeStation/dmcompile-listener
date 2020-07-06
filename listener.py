8ab25aab7af512d087e9a0616409e5eb0ecb3952
crossedfall <ping@crossedfall.io>
2020-07-06 01:56:14 -0500

    Adds the ability to build new test images
    If the listener is sent a BYOND version that isn't already covered in the list of images, it'll automatically build a new image for that version.

--- listener.py
+++ listener.py
@@ -4,10 +4,12 @@ import string
 import shutil
 import time
 import platform
+import docker
 from flask import Flask, jsonify, request, abort
 from pathlib import Path
 
 app = Flask(__name__)
+client = docker.from_env()
 
 CODE_FILE = Path.cwd().joinpath("templates/code.dm")
 HOST = "127.0.0.1"
@@ -44,7 +46,38 @@ def randomString(stringLength=24):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
+def checkVersions(version:str):
+    try:
+        tags_list = client.images.list(name='test')[0].tags
+    except IndexError:
+        return False
+
+    if f"test:{version}" in tags_list:
+        return True
+    else:
+        return False
+
+def buildVersion(version:str):
+    #Check if the version is already built
+    if checkVersions(version=version):
+        return
+    else:
+        try:
+            print(f"Attempting to build version: {version}")
+            return client.images.build(path=f'{Path.cwd()}', dockerfile='Dockerfile', rm=True, pull=True, tag=f'test:{version}', buildargs={'BYOND_VERSION':version})
+        except docker.errors.BuildError:
+            raise
+
 def compileTest(codeText:str, version:str):
+    try:
+        buildVersion(version=version)
+    except docker.errors.BuildError as e:
+        results = {
+            "build_error": True,
+            "exception": str(e) 
+        }
+        return results
+
     randomDir = Path.cwd().joinpath(randomString())
     randomDir.mkdir()
     shutil.copyfile(TEST_DME, randomDir.joinpath("test.dme"))
@@ -54,6 +87,7 @@ def compileTest(codeText:str, version:str):
         else:
             fc.write(loadTemplate(codeText, False))
     if HOST_OS == "Windows":
+        #To get cleaner outputs, we run docker as a subprocess rather than through the API
         proc = subprocess.Popen(["docker", "run", "--name", f"{randomDir.name}", "--rm", "-v", f"{randomDir}:/app/code:ro", f"test:{version}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
         proc = subprocess.Popen([f"{Path.home()}/bin/docker", "run", "--name", f"{randomDir.name}", "--rm", "-v", f"{randomDir}:/app/code:ro", f"test:{version}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -78,7 +112,8 @@ def compileTest(codeText:str, version:str):
 
     if f"Unable to find image 'test:{version}' locally" in errs:
         results = {
-            "build_error": True
+            "build_error": True,
+            "exception": errs
         }
     else:
         results = {
